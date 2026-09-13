acaffab55a2f7965695461fb32b678c70bccc504
x_msth_x <k67414684@gmail.com>
2026-09-13 18:11:27 +0300

    Add files via upload

---
+++ ex7.java
@@ -0,0 +1,10 @@
+public class clas2 {
+    public static void main(String[] args) {
+        int intVal = 250;
+        double doubleVal = intVal;
+
+        System.out.println("Результат:");
+        System.out.println("int: " + intVal);
+        System.out.println("douple: " + doubleVal);
+    }
+}
---
+++ ex8.java
@@ -0,0 +1,8 @@
+public class clas2 {
+    public static void main(String[] args) {
+        double priceWithPennies = 49.99;
+        int roundedPrice = (int) priceWithPennies;
+
+        System.out.println("Результат: " + roundedPrice);
+    }
+}
